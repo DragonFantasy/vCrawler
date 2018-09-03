ddb80d347b6734c9669b7d16cd56bac0caae1594
LAPTOP-RBAQ0G96\lenovo <xq6311@qq.com>
2018-09-03 15:32:59 +0800

    mod crawler

--- vCrawler.py
+++ vCrawler.py
@@ -5,4 +5,4 @@ import math
 for i in range(1000000,1000020):
     response = request.urlopen("https://movie.douban.com/celebrity/"+str(i))
     soup = BeautifulSoup(response)
-    print(soup.img['alt'])
+    print(soup.img['src'])
