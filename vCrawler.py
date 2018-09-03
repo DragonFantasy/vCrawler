6a1b0c1521f5423800422a9776a6b5fdc09f6780
DragonFantasy <xiagxnnlx@126.com>
2018-09-03 22:15:27 +0800

    add pymysql

--- vCrawler.py
+++ vCrawler.py
@@ -1,8 +1,29 @@
 from urllib import request
 from bs4 import BeautifulSoup
 import math
+import pymysql
 
-for i in range(1000000,1000020):
+config = {
+    'host':'115.28.168.166',
+    'port':3306,
+    'user':'axlk',
+    'password':'axlk123',
+    'db':'filmstar',
+    'charset':'utf8',
+    'cursorclass':pymysql.cursors.DictCursor,
+}
+db = pymysql.connect(**config)
+cursor = db.cursor()
+insert_arr = []
+for i in range(1000000,1000002):
     response = request.urlopen("https://movie.douban.com/celebrity/"+str(i))
     soup = BeautifulSoup(response)
-    print(soup.img['src'])
+    print(soup.img['alt'])
+    insert_arr.append((i, soup.img['alt'], soup.img['src']))
+try:
+    cursor.executemany("INSERT INTO star (star_id, star_name, img_url) values (%s,%s,%s)", insert_arr)
+    db.commit()
+except:
+    db.rollback()
+db.close()
+print("fin...")
