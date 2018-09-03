63ac38cc621b8eee9379cc18fb540b2a68d10a97
LAPTOP-RBAQ0G96\lenovo <xq6311@qq.com>
2018-09-03 14:32:26 +0800

    add beautifulsoup

--- vCrawler.py
+++ vCrawler.py
@@ -1,2 +1,8 @@
-str = "sssss"
-print(str)
+from urllib import request
+from bs4 import BeautifulSoup
+import math
+
+for i in range(1000000,1000020):
+    response = request.urlopen("https://movie.douban.com/celebrity/"+str(i))
+    soup = BeautifulSoup(response)
+    print(soup.img['alt'])
