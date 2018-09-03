600c618067d20ebafad6dd60b92984ac4d2e7f40
DragonFantasy <xiagxnnlx@126.com>
2018-09-03 22:28:34 +0800

    add sys.argv

--- vCrawler.py
+++ vCrawler.py
@@ -2,12 +2,13 @@ from urllib import request
 from bs4 import BeautifulSoup
 import math
 import pymysql
+import sys
 
 config = {
     'host':'115.28.168.166',
     'port':3306,
-    'user':'axlk',
-    'password':'axlk123',
+    'user':sys.argv[1],
+    'password':sys.argv[2],
     'db':'filmstar',
     'charset':'utf8',
     'cursorclass':pymysql.cursors.DictCursor,
@@ -21,7 +22,7 @@ for i in range(1000000,1000002):
     print(soup.img['alt'])
     insert_arr.append((i, soup.img['alt'], soup.img['src']))
 try:
-    cursor.executemany("INSERT INTO star (star_id, star_name, img_url) values (%s,%s,%s)", insert_arr)
+    cursor.executemany("INSERT INTO star (star_id, star_name, img_url) values (%s,%s,%s) ON DUPLICATE KEY UPDATE star_name=value(star_name),img_url=value(img_url)", insert_arr)
     db.commit()
 except:
     db.rollback()
