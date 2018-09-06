07977d2ed8b1124363e30df6aa55f09347d29214
LAPTOP-RBAQ0G96\lenovo <xq6311@qq.com>
2018-09-06 17:37:51 +0800

    devide into male stars & female stars

--- vCrawler.py
+++ vCrawler.py
@@ -1,8 +1,6 @@
 from urllib import request
 from bs4 import BeautifulSoup
-import math
-import pymysql
-import sys
+import math,pymysql,sys,re
 
 config = {
     'host':'115.28.168.166',
@@ -18,13 +16,24 @@ cursor = db.cursor()
 insert_arr = []
 for i in range(1000000,1000002):
     response = request.urlopen("https://movie.douban.com/celebrity/"+str(i))
-    soup = BeautifulSoup(response)
-    print(soup.img['alt'])
-    insert_arr.append((i, soup.img['alt'], soup.img['src']))
+    soup = BeautifulSoup(response, features="html.parser")
+    info_list = soup.select(".info")
+    sex = 0
+    for info in info_list:
+        li_list = info.find_all('li')
+        for li in li_list:
+            if "性别" in str(li):
+                if("男" in str(li)):
+                    sex = 1
+                break
+    insert_arr.append((i, soup.img['alt'], sex, soup.img['src']))
 try:
-    cursor.executemany("INSERT INTO star (star_id, star_name, img_url) values (%s,%s,%s) ON DUPLICATE KEY UPDATE star_name=value(star_name),img_url=value(img_url)", insert_arr)
+    cursor.executemany("INSERT INTO star (star_id, star_name, sex, img_url) VALUES (%s,%s,%s,%s) \
+     ON DUPLICATE KEY UPDATE star_name=VALUES(star_name),sex=VALUES(sex),img_url=VALUES(img_url)", insert_arr)
+    print(cursor)
     db.commit()
-except:
+except Exception as e:
+    print(repr(e))
     db.rollback()
 db.close()
 print("fin...")
